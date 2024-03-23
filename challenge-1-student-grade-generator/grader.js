51573e332f43417abceba1e34a7acfffdacaadcd
Ian Mworia <ianmworia156@gmail.com>
2024-03-23 11:21:49 +0300

    code challnge complete

--- challenge-1-student-grade-generator/grader.js
+++ challenge-1-student-grade-generator/grader.js
@@ -33,19 +33,28 @@ promptUser()
     for the condition checks 
 */
  function gradeGenerator(score){
+    
         let grade = ' '
+
         if  (score > 100 || score < 0) {
                 grade = 'Please enter a score btwn 0-100!!'
+
         } else if(score >= 79) {
                 grade = 'A'
+
         }else if(score < 79 && score >= 60) {
+
             grade = 'B'
         }else if(score <= 59 && score > 49){
+
             grade = 'C'
         }else if(score <= 49 && score > 40){
+
             grade = 'D'
         }else if(score<40){
+
             grade = 'E'
+
         }else{
             return 'please enter a valid score'
         }
